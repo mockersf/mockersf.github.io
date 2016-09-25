ae03d91433c2a5ea329061ecf004252e8b39c90b
François Mockers <f.mockers@ssi.samsung.com>
2016-09-25 04:54:57 +0200

    update style

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ layout: null
 ---
 $(document).ready(function () {
   $('a.blog-button').click(function (e) {
+    $('.about').addClass('hidden')
+    $('.main-post-list').removeClass('hidden')
     if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
     currentWidth = $('.panel-cover').width()
     if (currentWidth < 960) {
@@ -10,12 +12,32 @@ $(document).ready(function () {
       $('.content-wrapper').addClass('animated slideInRight')
     } else {
       $('.panel-cover').css('max-width', currentWidth)
-      $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
+      $('.panel-cover').animate({'max-width': '350px', 'width': '40%'}, 400, swing = 'swing', function () {})
+    }
+  })
+
+  $('a.about-button').click(function (e) {
+    $('.about').removeClass('hidden')
+    $('.main-post-list').addClass('hidden')
+    if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
+    currentWidth = $('.panel-cover').width()
+    if (currentWidth < 960) {
+      $('.panel-cover').addClass('panel-cover--collapsed')
+      $('.content-wrapper').addClass('animated slideInRight')
+    } else {
+      $('.panel-cover').css('max-width', currentWidth)
+      $('.panel-cover').animate({'max-width': '350px', 'width': '40%'}, 400, swing = 'swing', function () {})
     }
   })
 
   if (window.location.hash && window.location.hash == '#blog') {
     $('.panel-cover').addClass('panel-cover--collapsed')
+    $('.about').addClass('hidden')
+  }
+
+  if (window.location.hash && window.location.hash == '#about') {
+    $('.panel-cover').addClass('panel-cover--collapsed')
+    $('.main-post-list').addClass('hidden')
   }
 
   if (window.location.pathname !== '{{ site.baseurl }}' && window.location.pathname !== '{{ site.baseurl }}index.html') {
