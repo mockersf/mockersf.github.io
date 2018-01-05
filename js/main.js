8e59539c468f08f5704da7537dbc84a7cd265fd6
François Mockers <mockersf@gmail.com>
2018-01-06 00:47:55 +0100

    switched to font awesome for icons

--- js/main.js
+++ js/main.js
@@ -38,6 +38,7 @@ $(document).ready(function () {
   if (window.location.hash && window.location.hash == '#about') {
     $('.panel-cover').addClass('panel-cover--collapsed')
     $('.main-post-list').addClass('hidden')
+    $('.about').removeClass('hidden')
   }
 
   if (window.location.pathname !== '{{ site.baseurl }}' && window.location.pathname !== '{{ site.baseurl }}index.html') {
@@ -46,12 +47,12 @@ $(document).ready(function () {
 
   $('.btn-mobile-menu').click(function () {
     $('.navigation-wrapper').toggleClass('visible animated bounceInDown')
-    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
+    $('.btn-mobile-menu__icon').toggleClass('fa-bars fa-times-circle animated fadeIn')
   })
 
   $('.navigation-wrapper .blog-button').click(function () {
     $('.navigation-wrapper').toggleClass('visible')
-    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
+    $('.btn-mobile-menu__icon').toggleClass('fa-bars fa-times-circle animated fadeIn')
   })
 
 })
