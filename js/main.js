6e03dab5282e7c477edd0f86a67eda9b128d49a9
François Mockers <mockersf@gmail.com>
2020-07-19 01:43:26 +0200

    :wrench: fix usage of base url ?

--- js/main.js
+++ js/main.js
@@ -1,58 +1,17 @@
 ---
-layout: null
+  layout: null
 ---
-$(document).ready(function () {
-  $('a.blog-button').click(function (e) {
-    $('.about').addClass('hidden')
-    $('.main-post-list').removeClass('hidden')
-    if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
-    currentWidth = $('.panel-cover').width()
-    if (currentWidth < 960) {
-      $('.panel-cover').addClass('panel-cover--collapsed')
-      $('.content-wrapper').addClass('animated slideInRight')
-    } else {
-      $('.panel-cover').css('max-width', currentWidth)
-      $('.panel-cover').animate({'max-width': '200px', 'width': '25%'}, 400, swing = 'swing', function () {})
-    }
-  })
-
-  $('a.about-button').click(function (e) {
-    $('.about').removeClass('hidden')
-    $('.main-post-list').addClass('hidden')
-    if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
-    currentWidth = $('.panel-cover').width()
-    if (currentWidth < 960) {
-      $('.panel-cover').addClass('panel-cover--collapsed')
-      $('.content-wrapper').addClass('animated slideInRight')
-    } else {
-      $('.panel-cover').css('max-width', currentWidth)
-      $('.panel-cover').animate({'max-width': '200px', 'width': '25%'}, 400, swing = 'swing', function () {})
-    }
-  })
-
-//  if (window.location.hash && window.location.hash == '#blog') {
+  $(document).ready(function () {
     $('.panel-cover').addClass('panel-cover--collapsed')
-    $('.about').addClass('hidden')
-//  }
 
-  if (window.location.hash && window.location.hash == '#about') {
-    $('.panel-cover').addClass('panel-cover--collapsed')
-    $('.main-post-list').addClass('hidden')
-    $('.about').removeClass('hidden')
-  }
+    $('.btn-mobile-menu').click(function () {
+      $('.navigation-wrapper').toggleClass('visible animated bounceInDown')
+      $('.btn-mobile-menu__icon').toggleClass('fa-bars fa-times-circle animated fadeIn')
+    })
 
-  if (window.location.pathname !== '{{ site.baseurl }}' && window.location.pathname !== '{{ site.baseurl }}index.html') {
-    $('.panel-cover').addClass('panel-cover--collapsed')
-  }
+    $('.navigation-wrapper .blog-button').click(function () {
+      $('.navigation-wrapper').toggleClass('visible')
+      $('.btn-mobile-menu__icon').toggleClass('fa-bars fa-times-circle animated fadeIn')
+    })
 
-  $('.btn-mobile-menu').click(function () {
-    $('.navigation-wrapper').toggleClass('visible animated bounceInDown')
-    $('.btn-mobile-menu__icon').toggleClass('fa-bars fa-times-circle animated fadeIn')
   })
-
-  $('.navigation-wrapper .blog-button').click(function () {
-    $('.navigation-wrapper').toggleClass('visible')
-    $('.btn-mobile-menu__icon').toggleClass('fa-bars fa-times-circle animated fadeIn')
-  })
-
-})
